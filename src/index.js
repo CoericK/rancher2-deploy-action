2f3e57c5972b0baf7a6def473fbd78b8e24a494a
Erick Arroyo <erick.1726@gmail.com>
2022-05-02 00:55:31 -0500

    Starting Project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const core = require("@actions/core");
+const axios = require("axios");
+
+process.on("unhandledRejection", handleError);
+main().catch(handleError);
+
+async function main() {
+  const rancherUrl = core.getInput("rancher_url", { required: true });
+  const rancherToken = core.getInput("rancher_token", { required: true });
+  const clusterId = core.getInput("cluster_id", { required: true });
+  const namespace = core.getInput("namespace", { required: true });
+  const deployment = core.getInput("deployment", { required: true });
+  const dockerImage = core.getInput("docker_image", { required: true });
+
+  const headers = {
+    "Content-Type": "application/json-patch+json",
+    Authorization: `Basic ${rancherToken}`,
+  };
+
+  const config = {
+    method: "PATCH",
+    url: `${rancherUrl}/k8s/clusters/${clusterId}/apis/apps/v1/namespaces/${namespace}/deployments/${deployment}`,
+    headers,
+    data: `[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "${dockerImage}"}]`,
+  };
+  await axios(config).then(console.log).catch(console.error);
+}
+
+function handleError(err) {
+  console.log(err);
+  core.setFailed(err.message);
+}
